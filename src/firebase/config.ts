2efd539431708a2f3aee5f8d620cfcc4743660fb
jose colivoro <elmecaniqueador@gmail.com>
2025-11-14 00:43:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8431315151-41f4f",
+  "appId": "1:444802905508:web:7f608bffed844f64af7d31",
+  "apiKey": "AIzaSyCNluEV_MJym_0oKYPUlumt4gG5kDq6T14",
+  "authDomain": "studio-8431315151-41f4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "444802905508"
+};
